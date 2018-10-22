0616e7253b472d0bd37cf760f11398f7d5f40f08
Ben <benpong89@gmail.com>
2018-10-22 15:34:11 -0400

    update rspecs

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'renders all the users'
+  end
+end
---
+++ spec/features/user_features_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :feature do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
